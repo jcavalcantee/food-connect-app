8268076e84f0656fd9c322b3585deb7b615476c2
GabrielLomba12 <lombagabriel324@gmail.com>
2025-03-30 02:59:48 -0300

    feature: O usuário pode visualizar suas informações antes de editar.

--- src/components/FooterHome/index.tsx
+++ src/components/FooterHome/index.tsx
@@ -1,8 +1,16 @@
 import { View, TouchableOpacity, Text } from 'react-native';
 import { styles } from './styles';
+import { useNavigation } from '@react-navigation/native';
 import { MaterialIcons, Feather, Ionicons } from '@expo/vector-icons';
+import { NavigationProp } from '@react-navigation/native';
 
 export default function FooterHome() {
+
+type RootStackParamList = {
+  AlterUser: undefined; // Define the route and its parameters
+};
+
+const navigation = useNavigation<NavigationProp<RootStackParamList>>();
  return (
    <View style={styles.footer}>
          <TouchableOpacity style={styles.iconContainer}>
@@ -17,10 +25,13 @@ export default function FooterHome() {
               <Ionicons name="receipt-outline" size={30} color="white" />
               <Text style={styles.iconText}>PEDIDOS</Text>
          </TouchableOpacity>
-         <TouchableOpacity style={styles.iconContainer}>
-              <Feather name="user" size={30} color="white" />
-              <Text style={styles.iconText}>PERFIL</Text>
-         </TouchableOpacity>
+         <TouchableOpacity
+        style={styles.iconContainer}
+        onPress={() => navigation.navigate('AlterUser')} 
+      >
+        <Feather name="user" size={30} color="white" />
+        <Text style={styles.iconText}>PERFIL</Text>
+      </TouchableOpacity>
     </View>
   );
 }
