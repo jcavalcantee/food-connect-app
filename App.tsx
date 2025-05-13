0ca059b4e9c7e73f9cbed83e6b8ee06b8bcdeb5e
Lucas Gouveia <lucaspereiraa81@gmail.com>
2025-05-13 01:31:13 -0300

    Improving list order by user id

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
 import Routes from './src/routes';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
-    return <Routes />;
+  return (
+    <SafeAreaProvider>
+      <Routes />
+    </SafeAreaProvider>
+  );
 }
+
