0ffe49b927d26e98451808ea2c837192f72e7910
Lucas Gouveia <lucaspereiraa81@gmail.com>
2025-05-14 20:44:16 -0300

    Generating apk

--- android/app/src/main/java/br/com/senac/foodconnect/MainApplication.kt
+++ android/app/src/main/java/br/com/senac/foodconnect/MainApplication.kt
@@ -16,6 +16,9 @@ import com.facebook.soloader.SoLoader
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
 
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+import com.dieam.reactnativepushnotification.modules.RNPushNotificationHelper;
+
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
@@ -42,9 +45,9 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
+    RNPushNotificationHelper(this).createDefaultChannels()
     SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
