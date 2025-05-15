c359164e4576a90b1471c078d2906451a0d088fa
Lucas Gouveia <lucaspereiraa81@gmail.com>
2025-05-15 14:43:08 -0300

    back to env configuration

--- src/api/clients/Order.ts
+++ src/api/clients/Order.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// const ACCESS_IP_API = process.env.EXPO_PUBLIC_ACCESS_IP_API;
-const ACCESS_IP_API = "http://10.0.0.58";
+const ACCESS_IP_API = process.env.EXPO_PUBLIC_ACCESS_IP_API;
 
 const order = axios.create({
     baseURL: `${ACCESS_IP_API}:8085`,
